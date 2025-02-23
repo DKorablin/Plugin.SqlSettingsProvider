710a70b460e41a71ac61ec1d0144b65e219fac0a
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 17:21:49 +0300

    Unify plugins build

--- Plugin.SqlSettingsProvider/Properties/AssemblyInfo.cs
+++ Plugin.SqlSettingsProvider/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("30c832aa-37b3-4748-80c4-01604b5434ad")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=103")]
 #else
 
-[assembly: AssemblyTitle("Plugin.SqlSettingsProvider")]
 [assembly: AssemblyDescription("SQL Settings provider plugin")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.SqlSettingsProvider")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2012-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2012-2025")]
 #endif
